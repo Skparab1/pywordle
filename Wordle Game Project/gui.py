101dc1bc6eb16a19b3260cb06e5f12c27b3e6e13
Shubham <71990977+Skparab1@users.noreply.github.com>
2022-03-01 09:54:30 -0800

    really basic gui intro

--- Wordle Game Project/gui.py
+++ Wordle Game Project/gui.py
@@ -16,23 +16,7 @@ application_window = tk.Tk()
 
 answer = simpledialog.askstring("Input", "What is your first name?",
                                 parent=application_window)
-if answer is not None:
+if answer is not None or answer == '':
     print("Your name is", answer)
 else:
-    print("You don't have a first name?")
-
-answer = simpledialog.askinteger("Input", "What is your age?",
-                                parent=application_window,
-                                minvalue=0, maxvalue=100)
-if answer is not None:
-    print("Your age is ", answer)
-else:
-    print("You don't have an age?")
-
-answer = simpledialog.askfloat("Input", "What is your salary?",
-                               parent=application_window,
-                               minvalue=0.0, maxvalue=100000.0)
-if answer is not None:
-    print("Your salary is ", answer)
-else:
-    print("You don't have a salary?")
+    print("You don't have a name? ok...")
